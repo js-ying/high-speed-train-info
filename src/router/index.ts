91ce1131906436bc675130219cd4996ea56a1002
Kevin Ying <kevin.js.ying@ibm.com>
2021-10-29 02:24:20 +0800

    應該要加這個才會記住返回 keep-alive 時的高度但不知為何之前 firefox 不需要（但加了這個後就一定會平滑滾動了 QQ）

--- src/router/index.ts
+++ src/router/index.ts
@@ -34,7 +34,14 @@ const routes: Array<RouteRecordRaw> = [
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  }
 });
 
 export default router;
