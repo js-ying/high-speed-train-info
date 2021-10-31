5849f472cccf50de40f62add154b719de84cfb4f
Kevin Ying <kevin.js.ying@ibm.com>
2021-10-31 19:10:38 +0800

    update about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -30,6 +30,18 @@
 
         <div class="footer">
           <div id="time">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-info-circle-fill"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"
+              />
+            </svg>
             發行日：2021-11-11，最後更新：2021-11
           </div>
         </div>
@@ -62,5 +74,17 @@ export default defineComponent({
 <style lang="scss" scoped>
 small {
   font-size: 80%;
+}
+
+.footer {
+  font-size: 0.85em;
+  color: $taupe-gray;
+  margin-top: 2.5rem;
+
+  #time {
+    svg {
+      margin-right: 0.25rem;
+    }
+  }
 }
 </style>
